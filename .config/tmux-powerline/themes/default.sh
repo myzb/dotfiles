d1df4391e781432c5f07c9ee025e515157af7b8b
Matthias Y <myzb.dev@gmail.com>
2024-06-11 17:56:44 +0200

    DNM tmux-powerline coloring

--- .config/tmux-powerline/themes/default.sh
+++ .config/tmux-powerline/themes/default.sh
@@ -49,10 +49,14 @@ fi
 # shellcheck disable=SC2128
 if [ -z "$TMUX_POWERLINE_WINDOW_STATUS_FORMAT" ]; then
 	TMUX_POWERLINE_WINDOW_STATUS_FORMAT=(
-		"#[$(format regular)]"
+		"#[$(format inverse),bg=#3b4252]"
+		"$TMUX_POWERLINE_DEFAULT_LEFTSIDE_SEPARATOR"
+		"#[$(format regular),bg=color0]"
 		"  #I#{?window_flags,#F, } "
 		"$TMUX_POWERLINE_SEPARATOR_RIGHT_THIN"
 		" #W "
+		"#[$(format regular),fg=color0]"
+		"$TMUX_POWERLINE_DEFAULT_LEFTSIDE_SEPARATOR"
 	)
 fi
 
@@ -88,8 +92,7 @@ fi
 # shellcheck disable=SC1143,SC2128
 if [ -z "$TMUX_POWERLINE_LEFT_STATUS_SEGMENTS" ]; then
 	TMUX_POWERLINE_LEFT_STATUS_SEGMENTS=(
-		"tmux_session_info 148 234"
-		"hostname #bf616a #2e3444"
+		"tmux_session_info #88c0d0 #3b4252"
 		#"mode_indicator 165 0"
 		#"ifstat 30 255"
 		#"ifstat_sys 30 255"
@@ -122,7 +125,8 @@ if [ -z "$TMUX_POWERLINE_RIGHT_STATUS_SEGMENTS" ]; then
 		#"xkb_layout 125 117"
 		#"date_day 235 136"
 		"date #3b4252 #d8dee9 ${TMUX_POWERLINE_SEPARATOR_LEFT_BOLD}"
-		"time #81a1c1 #2e3444 ${TMUX_POWERLINE_SEPARATOR_LEFT_BOLD}"
+		"time #3b4252 #d8dee9 ${TMUX_POWERLINE_SEPARATOR_LEFT_THIN}"
+		"hostname #88c0d0 #3b4252"
 		#"utc_time 235 136 ${TMUX_POWERLINE_SEPARATOR_LEFT_THIN}"
 	)
 fi
