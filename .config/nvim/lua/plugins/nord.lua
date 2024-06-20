fd29ece8dec9a37ac82261c8b834c91f7dc9b895
Matthias Y <myzb.dev@gmail.com>
2024-06-20 11:37:14 +0200

    nvim: tweak nord highlights

--- .config/nvim/lua/plugins/nord.lua
+++ .config/nvim/lua/plugins/nord.lua
@@ -1,7 +1,11 @@
 return {
 	"gbprod/nord.nvim",
 	lazy = true,
-	opts = {},
+	opts = {
+		on_highlights = function(hl, c)
+			hl["TelescopePromptBorder"] = { link = "Label" }
+		end,
+	},
 	config = function(_, opts)
 		require("nord").setup(opts)
 	end,
