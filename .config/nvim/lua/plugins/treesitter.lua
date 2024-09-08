6efa826886c54366084841a21e09a432225ae39d
Matthias Y <myzb.dev@gmail.com>
2024-09-08 12:51:56 +0200

    nvim: add treesitter incr selection

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -23,6 +23,15 @@ return {
 			highlight = { enable = true },
 			indent = { enable = true },
 			endwise = { enable = true },
+			incremental_selection = {
+				enable = true,
+				keymaps = {
+					init_selection = "<c-space>",
+					node_incremental = "<c-space>",
+					scope_incremental = false,
+					node_decremental = "<bs>",
+				},
+			},
 		},
 		config = function(_, opts)
 			require("nvim-treesitter.configs").setup(opts)
