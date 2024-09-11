e059fa7af8d51957b4b320b79802097462b0b631
Matthias Y <myzb.dev@gmail.com>
2024-09-11 16:32:25 +0200

    nvim: lazy load mason on cmd

--- .config/nvim/lua/plugins/mason.lua
+++ .config/nvim/lua/plugins/mason.lua
@@ -1,5 +1,6 @@
 return {
 	"williamboman/mason.nvim",
+	cmd = { "Mason", "MasonInstall", "MasonUninstallAll", "MasonUpdate" },
 	dependencies = {
 		"williamboman/mason-lspconfig.nvim",
 		"WhoIsSethDaniel/mason-tool-installer.nvim",
