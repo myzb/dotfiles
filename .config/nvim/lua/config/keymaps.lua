51d46910dd07afa49aadea7f514c0f1808885c8f
Matthias Y <myzb.dev@gmail.com>
2024-09-12 22:30:54 +0200

    nvim: add keybind descriptions

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,10 +1,10 @@
 -- Navigate vim panes
 local keymap = vim.keymap.set
-local opts = {noremap = true, silent = true}
-keymap("n", "<c-k>", "<c-w>k", opts)
-keymap("n", "<c-j>", "<c-w>j", opts)
-keymap("n", "<c-h>", "<c-w>h", opts)
-keymap("n", "<c-l>", "<c-w>l", opts)
+-- local opts = {noremap = true, silent = true}
+-- keymap("n", "<c-k>", "<c-w>k", opts)
+-- keymap("n", "<c-j>", "<c-w>j", opts)
+-- keymap("n", "<c-h>", "<c-w>h", opts)
+-- keymap("n", "<c-l>", "<c-w>l", opts)
 
 -- Navigate within inisert mode
 keymap("i", "<c-k>", "<Up>", { desc = "Up" })
@@ -16,21 +16,21 @@ keymap("n", "<esc>", "<cmd> nohlsearch <cr>", { desc = "Hide search highlight" }
 keymap("n", "<leader>nr", "<cmd> set relativenumber! <cr>", { desc = "Toogle relative line numbers" })
 
 -- Change indent tab size
-vim.keymap.set('n', '<leader>ts', function()
-	vim.ui.input({ prompt = 'Indent tab to: ' }, function(input)
+vim.keymap.set("n", "<leader>it", function()
+	vim.ui.input({ prompt = "Indent tab to: " }, function(input)
 		vim.opt.tabstop = tonumber(input)
 		vim.opt.softtabstop = tonumber(input)
 		vim.opt.shiftwidth = tonumber(input)
 		vim.opt.expandtab = false
 	end)
-end)
+end, { desc = "Indent tabs" })
 
 -- Change indent space size
-vim.keymap.set('n', '<leader>ss', function()
-	vim.ui.input({ prompt = 'Indent space to: ' }, function(input)
+vim.keymap.set("n", "<leader>is", function()
+	vim.ui.input({ prompt = "Indent space to: " }, function(input)
 		vim.opt.tabstop = 8
 		vim.opt.softtabstop = tonumber(input)
 		vim.opt.shiftwidth = tonumber(input)
 		vim.opt.expandtab = true
 	end)
-end)
+end, { desc = "Indent spaces" })
