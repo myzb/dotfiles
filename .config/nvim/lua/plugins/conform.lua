8622ff440c9379d47c7982fd8e9c5bc9e692fa5d
Matthias Y <myzb.dev@gmail.com>
2025-05-20 21:20:43 +0200

    nvim: update formatter hotkey

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -6,7 +6,7 @@ return {
 	cmd = { "ConformInfo" },
 	keys = {
 		{
-			"<Leader>f",
+			"<Leader>cF",
 			function()
 				require("conform").format({ async = true, lsp_format = "fallback" })
 			end,
