a97ee26b4150b75b934c1d1b57fb4c106cf28747
Matthias Y <myzb.dev@gmail.com>
2025-06-13 11:39:45 +0200

    nvim: add hardtime

---
+++ .config/nvim/lua/plugins/hardtime.lua
@@ -0,0 +1,36 @@
+return {
+	{
+		"rcarriga/nvim-notify",
+		lazy = true,
+		opts = {
+			stages = "fade",
+		},
+	},
+	{
+		"m4xshen/hardtime.nvim",
+		dependencies = {
+			"MunifTanjim/nui.nvim",
+			"rcarriga/nvim-notify",
+		},
+		keys = {
+			{ "<Leader>H", function() require("hardtime").toggle() end, desc = "Hardtime", },
+		},
+		opts = {
+			enabled = true,
+			restriction_mode = "hint",
+			disable_mouse = false,
+			disabled_keys = {
+				["<Up>"] = false,
+				["<Down>"] = false,
+				["<Left>"] = false,
+				["<Right>"] = false,
+			},
+			callback = function(text)
+				require("notify")(text,	vim.log.levels.WARN, { title = "Hardtime", timeout = 3000 })
+			end,
+		},
+		config = function(_, opts)
+			require("hardtime").setup(opts)
+		end,
+	},
+}
