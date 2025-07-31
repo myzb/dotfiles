152546643e74981c6cccbd480162c4d00828f5a0
Matthias Y <myzb.dev@gmail.com>
2025-08-01 00:46:54 +0200

    nvim: remove copilot deps from copilot chat

--- .config/nvim/lua/plugins/copilot.lua
+++ .config/nvim/lua/plugins/copilot.lua
@@ -1,21 +1,12 @@
 return {
-	{
-		"zbirenbaum/copilot.lua",
-		cmd = "Copilot",
-		lazy = true,
-		opts = {},
+	"CopilotC-Nvim/CopilotChat.nvim",
+	dependencies = {
+		{ "nvim-lua/plenary.nvim", branch = "master" },
 	},
-	{
-		"CopilotC-Nvim/CopilotChat.nvim",
-		dependencies = {
-			{ "zbirenbaum/copilot.lua" },
-			{ "nvim-lua/plenary.nvim" },
-		},
-		cmd = "CopilotChat",
-		build = "make tiktoken",
-		cond = function()
-			return vim.fn.executable("make") == 1
-		end,
-		opts = {},
-	},
+	cmd = "CopilotChat",
+	build = "make tiktoken",
+	cond = function()
+		return vim.fn.executable("make") == 1
+	end,
+	opts = {},
 }
